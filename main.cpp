e4acbd296a6195b101fa0913cf26025543ae7e4f
Alex Hughes <hughes.13.alex@gmail.com>
2019-03-18 17:21:20 -0700

    Finished assignment

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 
  A program that records high-score data for a fictitious game. The program will ask the user to enter five names,
  and five scores. It will store the data in memory, and print it back out sorted by score. Then rewrite my most recent
- high scores program so that each name/score pair is stored in a struct named highscore.
+ high scores program so that each name/score pair is stored in a struct named highscore. The program asks the user to
+ enter the amount of scores, then asks for the name and the score of each player in the original amount entered. Once
+ that data is collected the program executes and returns the list of scores from highest to lowest.
  */
 
 
@@ -16,12 +18,19 @@
 using namespace std;
 
 
+
+
+
+
 struct highscore
 {
-
     int score;
     char name[24];
 };
+
+
+
+
 
 
 void initializeArrays(highscore scores[], int size);
@@ -53,23 +62,19 @@ void initializeArrays(highscore scores[], int size)
 
 void sortData(highscore scores[], int size)
 {
-    int i, j;
-    highscore minObj, temp;
-
     for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
         {
-            for (j = i + 1; j < size; j++)
+            if(scores[i].score < scores[j].score)
             {
-                if(scores[i].score > scores[j].score)
-                {
-                    temp = scores[i];
-                    scores[i]=scores[j];
-                    scores[j]= temp;
-
-                }
+                highscore temp = scores[i];
+                scores[i] = scores[j];
+                scores[j] = temp;
             }
         }
-        }
+    }
+}
 
 
 
@@ -78,6 +83,7 @@ void sortData(highscore scores[], int size)
 
 
 void displayData(highscore scores[], int size) {
+    cout << "Top Scores:" << endl;
     for (int i = 0; i < size; i++) {
         cout << scores[i].name << ": " << scores[i].score << endl;
     }
@@ -103,7 +109,29 @@ int main() {
 
     initializeArrays(obj, size);
 
-    displayData(obj, size);
-
     sortData(obj, size);
+
+    displayData(obj, size);
 }
+
+
+
+
+
+
+/* Output:
+How many scores will you enter?4
+Enter the name for the score #1: Alex
+Enter the score for the score #1: 13
+Enter the name for the score #2: Sasha
+Enter the score for the score #2: 15
+Enter the name for the score #3: Max
+Enter the score for the score #3: 19
+Enter the name for the score #4: Morgan
+Enter the score for the score #4: 8
+Top Scores:
+Max: 19
+Sasha: 15
+Alex: 13
+Morgan: 8
+*/
